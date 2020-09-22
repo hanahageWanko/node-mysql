6244a23e99da87ecf9de70738745e90f53e85974
nakagawa <237s10mimi@gmail.com>
2020-09-22 23:19:18 +0900

    add Select sentence

--- src/app.js
+++ src/app.js
@@ -2,15 +2,31 @@ const express = require('express');
 const port = require('./port');
 const DB = require('./mysql');
 const app = express();
-const mysql = require('mysql')
 
 DB.connect( err => {
-  if(err) throw err;
+  if(err) console.error(err);
   console.log('Connected');
-  DB.query('CREATE DATABASE express_db', (err, result) => {
-    if(err) throw err;
-    console.log('database created');
+  // データベース作成
+  DB.query(`CREATE DATABASE ${process.env.DB_NAME}`, (err, result) => {
+    if(err) {
+      // console.error(err);
+      return;
+    };
   })
+
+  // テーブルの作成
+  const createTableSql = `CREATE TABLE IF NOT EXISTS ${process.env.TABLE_NAME} (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL)`;
+  DB.query(createTableSql, (err, result) => {
+    if(err) console.error(err);
+  });
+  
+  // user
+  console.log('Get table');
+  const selectTableSql = `SELECT * from ${process.env.TABLE_NAME}`;
+  DB.query(selectTableSql, (err, result, fields) => {
+    if(err) console.error(err);
+    console.log(result);
+  });
 });
 
 app.get('/', (req,res) => res.send('Hello World!!!!!!'));
--- src/mysql.js
+++ src/mysql.js
@@ -4,7 +4,8 @@ require('dotenv').config()
 const DB = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 module.exports = DB;
