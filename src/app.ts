81efd5f11c6e536667d0db10def3525264cbd3c3
nakagawa <237s10mimi@gmail.com>
2020-09-24 23:55:47 +0900

    convert typescript & fix

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,22 @@
-import * as express from 'express';
-import * as port from './port';
-import * as DB from './mysql';
+import express from 'express';
+import * as mysql from 'mysql';
+require('dotenv').config();
+
+const port:number = 3000;
 const app = express();
+
+export interface DB {
+  host: string,
+  user: string,
+  password: string,
+  database: string
+}
+const DB = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
 
 DB.connect( err => {
   if(err) console.error(err);
@@ -35,16 +50,21 @@ DB.connect( err => {
 
 
 // テーブルの中身を表示
-app.get('/', (req,res) => {
+app.get('/', (request, response) => {
   const sql = `select * from ${process.env.TABLE_NAME}`
   DB.query(sql, (err, result, fields) => {
     if(err) console.error(err);
-    res.send(result);
+    response.send(result);
     // res.send(fields);
   });
 });
-
 // データの挿入
+// const insertSql = `INSERT INTO users(name, email) VALUES(?, ?)`;
+// let insertData:object = ['max', 'qqq@qqq.jp']
+// DB.query(insertSql, insertData, (err, result, fields) => {
+//   if(err) console.error(err);
+//   console.log(result);
+// });
 
 
 app.listen(port, () => console.log(`Ecample app lestening on port'${port}!`));
